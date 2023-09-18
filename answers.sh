0d5ddd42f3f84e2ca513f89ceac0afafc72a3e2a
Gabriel Jefferson Turner <turne474@data.cs.purdue.edu>
2023-09-18 17:30:08 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,47 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+rm Memes/SpongeBob/money.jpeg
 
 # Task 3
-
+mv Memes/'Star Wars'/fafsa.jpg Memes/SpongeBob
 
 # Task 4
-
+rm -r Homework/CS191 
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++
 
 # Task 7
-
+cp -r Programs/Java Homework/CS180 && mv Homework/CS180/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/Count.c Programs/Goodbye.c Programs/C
 
 # Task 9
-
+mv Programs/Name.py Programs/Space.py Programs/'Python 3'
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls ..
 
 # Task 12
 : '
 Type your vim/nano customizations below:
 
-
+set titlecolor black,blue    # COLOR_1 is the text, COLOR_2 is the backgrou>
+set errorcolor white,red
+set functioncolor cyan,black
+set speller
+set autoindent
 
 '
+
